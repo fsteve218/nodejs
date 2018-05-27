a2ec34cc215baf3b799030624544170c89f42f74
Steve F <fsteve218@gmail.com>
2018-05-27 00:38:50 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+d = console.log;
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+const maintenance = true;
+
+// Handlebars template engine
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper( 'getCurrentYear', () => new Date().getFullYear() );
+hbs.registerHelper( 'scream', (str) => str.toUpperCase() ); 
+
+// Create a server log
+app.use( (req,res,next) =>{
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`; 
+
+    d(log)
+
+    fs.appendFile( 'server.log', log+'\n' );
+    next();
+})
+
+// Maintanence mode
+app.use( (req,res,next) => {
+    if( req.url.startsWith('/css') ) return next();
+    if( maintenance ) {
+        res.statusCode = 503;
+        res.render( 'maintenance.hbs', {
+            title : 'Maintenance Mode',
+            message : 'Site is currently under maintanence!',
+            backSoon : 'We will be back shortly!'
+        } );
+    }
+})
+
+// Server static content
+app.use( express.static('/home/scuba/me/dist') );  
+
+/*
+app.get( '/', (req,res) => {
+    res.render( 'index.hbs', {
+        pageTitle : 'Home',
+        welcomeMessage : 'Welcome to my page',
+        currentYear : 2018
+    } );
+})
+
+app.get( '/about', (req,res) => {
+    res.render( 'about.hbs', {
+        pageTitle : 'About',
+        currentYear : 2018
+    } );
+})
+*/
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
